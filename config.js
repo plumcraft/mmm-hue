7731cc5d0f6644ca8d396737d35d454e7d665805
plumcraft <olivier.lebhard@gmail.com>
2017-01-29 21:35:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-{ module: 'mmm-hue', position: 'top_right', config: { bridgeip: 'ENV_HUE_BRIDGE_IP' , userid: 'ENV_HUE_USER_ID' ,colour: true}, classes: 'plumcraft' }
+{ module: 'mmm-hue', position: 'top_right', config: { bridgeip: 'ENV_HUE_BRIDGE_IP' , userid: 'ENV_HUE_USER_ID' ,colour: true}, classes: 'plumcraft' },
